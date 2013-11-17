9d446a4a1445a4bec6874809ba719294e30a41bd
arfan308 <areha308@gmail.com>
2013-11-17 11:59:31 -0500

    Add initial files

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from py import *
+
+app = Flask(__name__)
+app.config.from_object('py.config')
+
+env = app.jinja_env
+env.line_statement_prefix = '='
+env.globals.update(utils=utils)
+
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5007)
