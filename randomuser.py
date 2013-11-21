8390b439e0f762f29f3c04c02bd6d090e25cff84
Jason <jason@ubuntu.(none)>
2013-11-21 06:21:20 -0500

    Can parse out all necessary elements from results, haven't done anything with it yet

--- randomuser.py
+++ randomuser.py
@@ -13,12 +13,19 @@ request = Request('http://api.randomuser.me/0.2')
 try:
 	response = urlopen(request)
 	results = response.read()
-	#print results
-        results=results.lstrip('{"results":')
-        results=results.rstrip("}")
-        print results
+	
+        #results=results.lstrip('{"results":')
+        #results=results.rstrip("}")
+        #print results
         
-        print results[results.rfind("gender"):results.rfind("gender")+len("gender")]
+        list=["gender","name","location","email","password","md5_hash","sha1_hash","phone","cell","SSN","picture",'"seed']
+        
+        #gender = results[results.rfind("gender")+len("gender"):results.rfind("name")]
+        #gender=gender[3:-3]
+        #print gender
+        
+        for x in range(0,len(list)-1):
+            print results[results.rfind(list[x])+len(list[x]):results.rfind(list[x+1])][3:-3]
         user={}
         
 except URLError, e:
