2fef370af7e9bda1582ada358b21415295190324
Jason <jason@ubuntu.(none)>
2013-11-21 13:06:28 -0500

    Moved template.profile.html to templates and inserted variables, wrote profile() in app.py

--- app.py
+++ app.py
@@ -9,5 +9,11 @@ env = app.jinja_env
 env.line_statement_prefix = '='
 env.globals.update(utils=utils)
 
+@app.route("/")
+def profile():
+	user=getUser()
+	return render_template("template.profile.html",picture=user["picture"],name=user["name"],age=user["age"],email=user["email"])
+			       
+			       
 if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5007)
