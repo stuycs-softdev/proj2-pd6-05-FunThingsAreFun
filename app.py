324a56662424722a2fc5812136478f779e4e55b0
Jasper Lu <repsajrules@gmail.com>
2013-11-29 19:45:23 -0800

    app

--- app.py
+++ app.py
@@ -1,22 +1,18 @@
 from flask import Flask, render_template
-from py import *
 import randomuser
 
 app = Flask(__name__)
-app.config.from_object('py.config')
-
-env = app.jinja_env
-env.line_statement_prefix = '='
-env.globals.update(utils=utils)
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+	return render_template("template.profile.html")
+	#return render_template("index.html")
 
 @app.route("/profile")
 def profile():
-	user=getUser()
-	return render_template("template.profile.html",picture=user["picture"],name=user["name"],age=user["age"],email=user["email"])
+	#user=getUser()
+	#return render_template("template.profile.html",picture=user["picture"],name=user["name"],age=user["age"],email=user["email"])
+	return render_template("template.profile.html")
 			       
 			       
 if __name__ == '__main__':
